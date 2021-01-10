220449134144acf31c4c635d6d7e3d8c9ccbd835
Nelson Mei <nelsonmei@Nelsons-MacBook-Pro.local>
2021-01-10 15:14:40 -0500

    enable all cors origins to access lambda

--- lambda/graphql.js
+++ lambda/graphql.js
@@ -1,7 +1,28 @@
-exports.handler = async function () {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'text/plain' },
-    body: `Hello, world!`,
-  };
+const { ApolloServer, gql } = require('apollo-server-lambda');
+const typeDefs = `
+    type Query {
+        hello: String
+    }
+`;
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
 };
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+
+exports.handler = server.createHandler({
+  cors: {
+    origin: '*',
+    methods: 'POST',
+    allowedHeaders: [
+      'Content-Type',
+      'Origin',
+      'Accept'
+    ]
+  }
+});
